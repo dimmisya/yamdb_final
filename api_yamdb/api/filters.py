b01a8bb0c3cef5f87b4def842731bd44a0ca1287
Дмитрий Мисюра <dimmis.ya@gmail.com>
2022-12-12 11:58:43 +0500

    re-try

---
+++ api_yamdb/api/filters.py
@@ -0,0 +1,25 @@
+import django_filters
+from django.db import models
+from django_filters import filters
+from reviews.models import Title
+
+
+class TitleFilter(django_filters.FilterSet):
+    category = filters.CharFilter(
+        field_name='category__slug'
+    )
+    genre = filters.CharFilter(
+        field_name='genre__slug'
+    )
+
+    class Meta:
+        model = Title
+        fields = ['name', 'year', 'genre', 'category']
+        filter_overrides = {
+            models.CharField: {
+                'filter_class': django_filters.CharFilter,
+                'extra': lambda f: {
+                    'lookup_expr': 'icontains',
+                },
+            },
+        }
